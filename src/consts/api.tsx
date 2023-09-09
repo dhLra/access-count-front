a6ff62b3c85707c7c2ce411a87bcea7662fba705
Dhiego <dhiegolira11@gmail.com>
2023-09-09 10:02:14 -0300

    change the api const

--- src/consts/api.tsx
+++ src/consts/api.tsx
@@ -1,2 +1,2 @@
-//export const api = 'https://room-maneger-api-3qo7y2nenq-ue.a.run.app'
-export const api = 'http://localhost:3000'
+export const api = 'https://access-count-api-test-3qo7y2nenq-ue.a.run.app'
+//export const api = 'http://localhost:3000'
